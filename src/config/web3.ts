ab6b5cc468b7e6e1aa2876f9f1ca139c34db16e0
Muhammad Al-Atrash <m.h.alatrah@gmail.com>
2024-12-08 18:49:31 -0500

    basic Web3 setup with MetaMask wallet connection

---
+++ src/config/web3.ts
@@ -0,0 +1,14 @@
+'use client'
+import { createConfig, http } from 'wagmi'
+import { sepolia } from 'wagmi/chains'
+import { injected } from 'wagmi/connectors'
+
+export const config = createConfig({
+  chains: [sepolia],
+  connectors: [
+    injected(),
+  ],
+  transports: {
+    [sepolia.id]: http()
+  }
+})
