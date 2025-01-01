d3dbb976312d6390c67fe79bb99bbdea5de1e90f
Muhammad Al-Atrash <m.h.alatrash@gmail.com>
2025-01-01 10:33:15 -0500

    initial Foundry tests and deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,1 +1,20 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
+import "forge-std/Script.sol";
+import "../src/ArtistToken.sol";
+
+contract Deploy is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        new ArtistToken(
+            "Test Token",
+            "TEST",
+            1_000_000 ether,
+            msg.sender
+        );
+
+        vm.stopBroadcast();
+    }
+}
--- test/ArtistToken.t.sol
+++ test/ArtistToken.t.sol
@@ -1,1 +1,29 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "forge-std/Test.sol";
+import "../src/ArtistToken.sol";
+
+contract ArtistTokenTest is Test {
+    ArtistToken public token;
+    address public owner;
+
+    function setUp() public {
+        owner = makeAddr("owner");
+        vm.prank(owner);
+        token = new ArtistToken(
+            "Test Token",
+            "TEST",
+            1_000_000 ether,
+            owner
+        );
+    }
+
+    function testDeployment() public {
+        assertEq(token.name(), "Test Token");
+        assertEq(token.symbol(), "TEST");
+        assertEq(token.totalSupply(), 1_000_000 ether);
+        assertEq(token.owner(), owner);
+    }
+}
